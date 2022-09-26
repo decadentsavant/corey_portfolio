79bafc93007bff256e179dd2b52a7e33e97c0c9f
Corey Stewart <decadentsavant@gmail.com>
2022-09-26 05:28:20 -0400

    Wire up providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,39 @@ import 'package:corey_portfolio/global_layout/portfolio_colors.dart';
 import 'package:corey_portfolio/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors = ref.watch(portfolioColorsProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Corey Portfolio',
       // we are using dark theme and we modify it as our need
       theme: ThemeData.dark().copyWith(
-        primaryColor: const PortfolioColors().primaryColor,
-        scaffoldBackgroundColor: const PortfolioColors().bgColor,
-        canvasColor: const PortfolioColors().bgColor,
+        primaryColor: colors.primaryColor,
+        scaffoldBackgroundColor: colors.bgColor,
+        canvasColor: colors.bgColor,
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
             .apply(bodyColor: Colors.white)
             .copyWith(
               bodyText1: TextStyle(
-                color: const PortfolioColors().bodyTextColor,
+                color: colors.bodyTextColor,
               ),
               bodyText2: TextStyle(
-                color: const PortfolioColors().bodyTextColor,
+                color: colors.bodyTextColor,
               ),
             ),
       ),
--- lib/screens/side_menu/side_menu_screen.dart
+++ lib/screens/side_menu/side_menu_screen.dart
@@ -1,3 +1,4 @@
+import 'package:corey_portfolio/global_layout/portfolio_colors.dart';
 import 'package:corey_portfolio/global_layout/ui_constants.dart';
 import 'package:corey_portfolio/screens/side_menu/side_menu_components/contact_information.dart';
 import 'package:corey_portfolio/screens/side_menu/side_menu_components/download_cv.dart';
@@ -5,14 +6,16 @@ import 'package:corey_portfolio/screens/side_menu/side_menu_components/side_menu
 import 'package:corey_portfolio/screens/side_menu/side_menu_components/sized_box_and_divider.dart';
 import 'package:corey_portfolio/screens/side_menu/side_menu_components/socials.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SideMenu extends StatelessWidget {
+class SideMenu extends ConsumerWidget {
   const SideMenu({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final colors = ref.watch(portfolioColorsProvider);
     return Drawer(
       child: SafeArea(
         child: Column(
@@ -22,16 +25,19 @@ class SideMenu extends StatelessWidget {
               child: SingleChildScrollView(
                 padding: const EdgeInsets.all(defaultPadding),
                 child: Column(
-                  children: const <Widget>[
-                    ContactInformation(),
-                    SizedBoxAndDivider(),
-                    DownloadCV(),
-                    Socials(),
-                    SizedBoxAndDivider(),
-                    // ElevatedButton(
-                    //   onPressed: PortfolioColors().toggle,
-                    //   child: const Text('Toggle Colors'),
-                    // ),
+                  children: <Widget>[
+                    const ContactInformation(),
+                    const SizedBoxAndDivider(),
+                    const DownloadCV(),
+                    const Socials(),
+                    const SizedBoxAndDivider(),
+                    ElevatedButton(
+                      onPressed: () {
+                        ref.read(portfolioColorsProvider.notifier).toggle();
+                        ref.read(portfolioColorsProvider).toString();
+                      },
+                      child: const Text('Toggle Colors'),
+                    ),
                   ],
                 ),
               ),
